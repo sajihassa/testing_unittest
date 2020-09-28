20e71a5a059b162e3a2050cd12abf2dd14346de9
ging <gging2105@gmail.com>
2020-09-28 12:48:13 +0700

    -test

--- src/test/java/com/unittest/testing/AppTest.java
+++ src/test/java/com/unittest/testing/AppTest.java
@@ -12,158 +12,264 @@ public class AppTest
 	@Test
 	public void testResult0input0() {
 		double input = 0;
-		double expected = 0;
-		object.input(input);
-		double actual = object.getCelcius();
-		assertEquals(expected,actual, 2);
+		boolean expected = true;
+		boolean actual = object.isBound(input);
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testResult1input1() {
+		double input = 1;
+		boolean expected = true;
+		boolean actual = object.isBound(input);
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testResultNegative1inputNegative1() {
+		double input = -1;
+		boolean expected = true;
+		boolean actual = object.isBound(input);
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testResult499input499() {
+		double input = 499;
+		boolean expected = true;
+		boolean actual = object.isBound(input);
+		assertEquals(expected,actual);
+	}
+ 	@Test
+	public void testResult837inputNegative499() {
+		double input = 499;
+		boolean expected = true;
+		boolean actual = object.isBound(input);
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testResult500input500() {
+		double input = 500;
+		boolean expected = true;
+		boolean actual = object.isBound(input);
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testResultNegative500inputNegative500() {
+		double input = -500;
+		boolean expected = true;
+		boolean actual = object.isBound(input);
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testResult501input501() {
+		double input = 501;
+		boolean expected = true;
+		boolean actual = object.isBound(input);
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testResultNegative501inputNegative501() {
+		double input = -501;
+		boolean expected = true;
+		boolean actual = object.isBound(input);
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testResult999input999() {
+		double input = 999;
+		boolean expected = true;
+		boolean actual = object.isBound(input);
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testResultNegative999inputNegative999() {
+		double input = -999;
+		boolean expected = true;
+		boolean actual = object.isBound(input);
+		assertEquals(expected,actual);
 	}
 	@Test
 	public void testResult1000input1000() {
 		double input = 1000;
-		double expected = 1000;
-		object.input(input);
-		double actual = object.getCelcius();
-		assertEquals(expected,actual, 2);
-	}
-	@Test
-	public void testResultMinus1000inputMinus1000() {
-		double input = -1000;
-		double expected = -1000;
-		object.input(input);
-		double actual = object.getCelcius();
-		assertEquals(expected,actual, 2);
-	}
-	@Test
-	public void testResultTrueinput0() {
-		double input = 0;
 		boolean expected = true;
 		boolean actual = object.isBound(input);
 		assertEquals(expected,actual);
 	}
 	@Test
-	public void testResultTrueinput1000() {
-		double input = 1000;
-		boolean expected = true;
-		boolean actual = object.isBound(input);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testResultTrueinputMinus1000() {
+	public void testResultNegative1000inputNegative1000() {
 		double input = -1000;
 		boolean expected = true;
 		boolean actual = object.isBound(input);
 		assertEquals(expected,actual);
 	}
+
+	
 	@Test
-	public void testResultTrueinput2000() {
-		double input = 2000;
-		boolean expected = false;
-		boolean actual = object.isBound(input);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testResultTrueinputMinus2000() {
-		double input = -2000;
-		boolean expected = false;
-		boolean actual = object.isBound(input);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testResultTrueinput1001() {
+	public void testResult1001input1001() {
 		double input = 1001;
 		boolean expected = false;
 		boolean actual = object.isBound(input);
 		assertEquals(expected,actual);
 	}
 	@Test
-	public void testResultTrueinputMinus1001() {
+	public void testResultNegative1001inputNegative1001() {
 		double input = -1001;
 		boolean expected = false;
 		boolean actual = object.isBound(input);
 		assertEquals(expected,actual);
 	}
 	@Test
-	public void testResultTrueinput1010() {
+	public void testResult1010input1010() {
 		double input = 1010;
 		boolean expected = false;
 		boolean actual = object.isBound(input);
 		assertEquals(expected,actual);
 	}
 	@Test
-	public void testResultTrueinputMinus1010() {
+	public void testResultNegative1010inputNegative1010() {
 		double input = -1010;
 		boolean expected = false;
 		boolean actual = object.isBound(input);
 		assertEquals(expected,actual);
 	}
 	@Test
-	public void testResultTrueinput999() {
-		double input = 999;
-		double expected = 999;
+	public void testResult2000input2000() {
+		double input = 2000;
+		boolean expected = false;
 		boolean actual = object.isBound(input);
 		assertEquals(expected,actual);
 	}
 	@Test
-	public void testResultTrueinputMinus999() {
-		double input = -999;
-		double expected = -999;
+	public void testResultNegative2000inputNegative2000() {
+		double input = -2000;
+		boolean expected = false;
 		boolean actual = object.isBound(input);
 		assertEquals(expected,actual);
 	}
-	@Test
-	public void testResultTrueinput501() {
-		double input = 501;
-		double expected = 501;
-		boolean actual = object.isBound(input);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testResultTrueinputMinus501() {
-		double input = -501;
-		double expected = -501;
-		boolean actual = object.isBound(input);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testResultTrueinput500() {
-		double input = 500;
-		double expected = 500;
-		boolean actual = object.isBound(input);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testResultTrueinputMinus500() {
-		double input = -500;
-		double expected = -500;
-		boolean actual = object.isBound(input);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testResultTrueinput499() {
-		double input = 499;
-		double expected = 499;
-		boolean actual = object.isBound(input);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testResultTrueinputMinus499() {
-		double input = -499;
-		double expected = -499;
-		boolean actual = object.isBound(input);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testResultTrueinput1() {
-		double input = 1;
-		double expected = 1;
-		boolean actual = object.isBound(input);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testResultTrueinputMinus1() {
-		double input = -1;
-		double expected = -1;
-		boolean actual = object.isBound(input);
-		assertEquals(expected,actual);
-	}
+		@Test
+		public void testResult00input00() {
+			double input = 0;
+			double expected =0;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult01input01() {
+			double input = 1;
+			double expected =1;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult00inputNegative01() {
+			double input = -1;
+			double expected = -1;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult0input499() {
+			double input = 499;
+			double expected = 499;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult499inputNegative499() {
+			double input = -499;
+			double expected = -499;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult0input500() {
+			double input = 500;
+			double expected = 500;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult0inputNegative500() {
+			double input = -500;
+			double expected = -500;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult0input501() {
+			double input = 501;
+			double expected = 501;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult0inputNegative501() {
+			double input = -501;
+			double expected = -501;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult0input999() {
+			double input = 999;
+			double expected = 999;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult0inputNegative999() {
+			double input = -999;
+			double expected = -999;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult0input1001() {
+			double input = 1001;
+			double expected = 1001;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult0inputNegative1001() {
+			double input = -1001;
+			double expected = -1001;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult0input1010() {
+			double input = 1010;
+			double expected = 1010;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult0input2000() {
+			double input = 2000;
+			double expected = 2000;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult2000inputNegative2000() {
+			double input = -2000;
+			double expected = -2000;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
 }
